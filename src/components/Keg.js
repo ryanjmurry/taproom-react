1192b206cf731ecc799394e1d4d613c97c97914d
Ryan Murry <ryanjmurry@gmail.com>
2018-09-07 16:04:35 -0700

    add linear progress bar showing remaining kegs

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const progressStyle = {
+  height: '20px',
+  color: 'red'
+}
 
 const Keg = (props) => {
   const keg = props
+  const abv = parseInt(props.abv);
+  const pintsRemaining = ((props.remaining / 124) * 100);
   return (
     <div>
+      <LinearProgress 
+        variant="determinate" 
+        value={pintsRemaining}
+      />
       {keg.name}
       {keg.brewer}
       {keg.description}
