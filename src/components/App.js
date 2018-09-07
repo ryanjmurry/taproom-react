44157f596c58746ffbd02b8897b85aea5454aa54
Ryan Murry <ryanjmurry@gmail.com>
2018-09-07 14:28:57 -0700

    add taplist and keg form components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
+import TapList from './TapList';
+import NewKegForm from './NewKegForm';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Nav />
+        <TapList />
+        <NewKegForm />
       </div>
     );
   }
