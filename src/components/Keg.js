98861c32ab30aa10cfe0440272c7cb7f0088740c
Ryan Murry <ryanjmurry@gmail.com>
2018-09-07 16:18:33 -0700

    add card and button imports from material-ui, add basic styling, flex edit button right

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import Edit from '@material-ui/icons/Edit';
 
 const progressStyle = {
-  height: '40px',
+  height: '20px',
+  margin: '20px'
+}
+
+const cardStyle = {
+  margin: '20px'
 }
 
 const Keg = (props) => {
@@ -12,18 +22,30 @@ const Keg = (props) => {
   const pintsRemaining = ((props.remaining / 124) * 100);
   return (
     <div>
-      Pints Remaining
+      <Card style={cardStyle}>
       <LinearProgress style={progressStyle}
         variant="determinate" 
         value={pintsRemaining}
       />
+      <CardContent>
       {keg.name}
       {keg.brewer}
       {keg.description}
       {keg.abv}
       {keg.price}
       {keg.remaining}
-      <hr/>
+      </CardContent>
+      <CardActions>
+        <div style={{ flex: 1 }}>
+          <Button size="small" variant="contained" color="primary">Sell Pint</Button>
+          <Button size="small">Sell Growler</Button>
+          <Button size="small">Sell Large Growler</Button>
+        </div>
+        <div>
+          <Button size="small"><Edit/></Button>
+        </div>
+      </CardActions>
+    </Card>
     </div>
   );
 }
