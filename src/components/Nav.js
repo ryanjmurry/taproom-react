854b6c7af7ab87535ea5b5bccee50df43849d38d
Ryan Murry <ryanjmurry@gmail.com>
2018-09-07 14:11:21 -0700

    create appbar with tabs using material ui components

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-const Nav = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+
+class Nav extends Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            centered
+          >
+            <Tab label="TapList" />
+            <Tab label="New Keg" />
+          </Tabs>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
 export default Nav;
