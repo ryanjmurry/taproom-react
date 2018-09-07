5fe69ef7364a400add4f74872da899f7688a3661
Ryan Murry <ryanjmurry@gmail.com>
2018-09-07 15:37:09 -0700

    add switch component to match path

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import Nav from './Nav';
 import TapList from './TapList';
 import NewKegForm from './NewKegForm';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Nav />
-        <TapList />
-        <NewKegForm />
+        <Switch>
+          <Route exact path='/' component={TapList} />
+          <Route path='/newkeg' component={NewKegForm} />
+        </Switch>
       </div>
     );
   }
