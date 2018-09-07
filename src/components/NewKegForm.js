63dc764ed3db3ebbdffb0a458f61d8bacdfdbfab
Ryan Murry <ryanjmurry@gmail.com>
2018-09-07 15:16:28 -0700

    add new keg form

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 const NewKegForm = () => {
+
+  const style={
+    color: 'red'
+  }
   return (
     <div>
-      More Data
+      <form>
+        <TextField style={style} 
+          id="name"
+          label="name"
+          margin="normal"
+        />
+        <TextField style={style} 
+          id="brewer"
+          label="brewer"
+          margin="normal"
+        />
+        <TextField style={style} 
+          id="description"
+          label="description"
+          margin="normal"
+        />
+        <TextField style={style} 
+          id="abv"
+          label="abv"
+          margin="normal"
+        />
+        <TextField style={style} 
+          id="price"
+          label="price"
+          margin="normal"
+        />
+        <TextField style={style} 
+          id="remaining"
+          label="remaining"
+          margin="normal"
+        />
+      </form>
     </div>
   );
 }
