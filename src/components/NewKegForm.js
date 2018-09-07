f99374b3efecfc435a68db299870b9f03238212f
Ryan Murry <ryanjmurry@gmail.com>
2018-09-07 14:28:47 -0700

    create new component

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewKegForm = () => {
+  return (
+    <div>
+      More Data
+    </div>
+  );
+}
+
+export default NewKegForm;
--- src/components/TapList.js
+++ src/components/TapList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TapList = () => {
   return (
     <div>
-      
+      Some Data
     </div>
   );
 }
